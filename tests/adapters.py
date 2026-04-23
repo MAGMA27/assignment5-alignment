389f480c6c37aaf7e3d7b524b386ca24fd6cc2d7
MAGMA27 <785783195@qq.com>
2026-04-23 14:21:21 +0800

    ep4: compute_entropy

--- tests/adapters.py
+++ tests/adapters.py
@@ -8,6 +8,7 @@ from torch import Tensor
 from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizerBase
 from cs336_alignment.tokenize_prompt_and_output import tokenize_prompt_and_output
+from cs336_alignment.compute_entropy import compute_entropy
 
 
 def run_tokenize_prompt_and_output(
@@ -84,6 +85,7 @@ def run_compute_group_normalized_rewards(
 
 def run_compute_entropy(logits: torch.Tensor) -> torch.Tensor:
     """Get the entropy of the logits (i.e., entropy of the final dimension)."""
+    return compute_entropy(logits)
     raise NotImplementedError
 
 
