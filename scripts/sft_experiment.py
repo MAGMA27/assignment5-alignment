1fef9ebe7270065d47f72e30da27f25faa2c93d0
MAGMA27 <785783195@qq.com>
2026-04-24 18:07:18 +0800

    ep7: compute_group_normalized_rewards

---
+++ scripts/sft_experiment.py
@@ -0,0 +1,64 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from vllm.model_executor import set_random_seed as vllm_set_random_seed
+from vllm import LLM, SamplingParams
+from transformers import AutoModel, AutoTokenizer, PreTrainedModel
+from unittest.mock import patch
+import wandb
+
+
+def init_vllm(model_id: str, device: str, seed: int, gpu_memory_utilization: float = 0.85):
+    """
+    Start the inference process, here we use vLLM to hold a model on
+    a GPU separate from the policy.
+    """
+    vllm_set_random_seed(seed)
+    # Monkeypatch from TRL:
+    # https://github.com/huggingface/trl/blob/
+    # 22759c820867c8659d00082ba8cf004e963873c1/trl/trainer/grpo_trainer.py
+    # Patch vLLM to make sure we can
+    # (1) place the vLLM model on the desired device (world_size_patch) and
+    # (2) avoid a test that is not designed for our setting (profiling_patch).
+    world_size_patch = patch("torch.distributed.get_world_size", return_value=1)
+    profiling_patch = patch(
+    "vllm.worker.worker.Worker._assert_memory_footprint_increased_during_profiling",
+    return_value=None
+    )
+    with world_size_patch, profiling_patch:
+        return LLM(
+            model=model_id,
+            device=device,
+            dtype=torch.bfloat16,
+            enable_prefix_caching=True,
+            gpu_memory_utilization=gpu_memory_utilization,
+        )
+
+def load_policy_into_vllm_instance(policy: PreTrainedModel, llm: LLM):
+    """
+    Copied from https://github.com/huggingface/trl/blob/
+    22759c820867c8659d00082ba8cf004e963873c1/trl/trainer/grpo_trainer.py#L670.
+    """
+    state_dict = policy.state_dict()
+    llm_model = llm.llm_engine.model_executor.driver_worker.model_runner.model
+    llm_model.load_weights(state_dict.items())
+
+
+if __name__ == '__main__':
+    # Setup wandb metrics
+    wandb.define_metric("train_step") # the x‑axis for training
+    wandb.define_metric("eval_step") # the x‑axis for evaluation
+    # everything that starts with train/ is tied to train_step
+    wandb.define_metric("train/*", step_metric="train_step")
+    # everything that starts with eval/ is tied to eval_step
+    wandb.define_metric("eval/*", step_metric="eval_step")
+
+    for inputs, labels in data_loader:
+        # Forward pass.
+        logits = model(inputs)
+        loss = loss_fn(logits, labels)
+        # Backward pass.
+        loss.backward()
+        # Update weights.
+        optimizer.step()
+        # Zero gradients in preparation for next iteration.
+        optimizer.zero_grad()
--- tests/adapters.py
+++ tests/adapters.py
@@ -12,6 +12,7 @@ from cs336_alignment.compute_entropy import compute_entropy
 from cs336_alignment.get_response_log_probs import get_response_log_probs
 from cs336_alignment.masked_normalize import masked_normalize
 from cs336_alignment.sft_microbatch_train_step import sft_microbatch_train_step
+from cs336_alignment.compute_group_normalized_rewards import compute_group_normalized_rewards
 
 
 def run_tokenize_prompt_and_output(
@@ -83,6 +84,12 @@ def run_compute_group_normalized_rewards(
                 You may choose what you wish to log here
                 (some statistics of the rewards, etc.).
     """
+    return compute_group_normalized_rewards(reward_fn,
+    rollout_responses,
+    repeated_ground_truths,
+    group_size,
+    advantage_eps,
+    normalize_by_std)
     raise NotImplementedError
 
 
